4bae628a3b46fa382031750fe7f65a6098851b84
Sehaj deep <Dsahej2004@outlook.com>
2025-12-11 18:19:36 +0530

    this is the commit for ratingapp

---
+++ RatingApplication/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import ContentRatings from './Components/contentRatings';
+
+function App() {
+  return(
+    <ContentRatings />
+  )
+}
+
+export default App;
---
+++ RatingApplication/src/Components/contentRatings.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import './componentRatings.css';
+
+class contentRatings extends React.Component {
+
+     constructor() {
+    super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+      }
+  }
+    }
+    render() {
+        return (
+            <div className="content-rating">
+                <p>
+                    this is the text
+                </p>
+                <div className="rating-buttons">
+                    <button className="likeButton" onClick={this.state.handleLike}>Like({this.state.likes})</button>
+                    <button className="dislikeButton" onClick={this.state.handleDislike}>Dislike({this.state.dislikes})</button>
+                </div>
+            </div>
+        );
+    }
+}
+export default contentRatings;
